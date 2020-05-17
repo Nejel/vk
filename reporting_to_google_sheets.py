4c32ba56278241ca7b7d68283027277810cb4423
nejel <nejel@mail.ru>
2020-05-18 00:39:33 +0300

    small fix

--- reporting_to_google_sheets.py
+++ reporting_to_google_sheets.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -18,13 +20,8 @@ t = 3600 #sec
 
 '''
 #TODO 
-1. Добавить таймдельту на последнюю неделю +
-2. Добавить LastUpdated +
-3. Поправить авторизацию через OAuth2
-4. Добавить новые статистики (?) https://vk.com/dev/stats.getPostReach +
-5. Записанное время в постах отстает на 7 часов +
-6. Проверка времени поста не реализована для уже записанных ранее в гугл-листы постов
-7. Добавить оценку количества комментов
+1. Fix OAuth2
+2. Quantity of comments
 
 '''
 
@@ -32,29 +29,26 @@ t = 3600 #sec
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1xZXg3o88qFKR1M9qOUtTTzE7AsZ_i_fyZNut8EOfyEc'  #  1gB5ycG0ts7vJgM3g9fL8RsR_PU8rEFk-Rns5Vec4ezo
-SAMPLE_RANGE_NAME = 'Sheet1!A3:G' #only for read previous posts
+SAMPLE_SPREADSHEET_ID = '1xZXg3o88qFKR1M9qOUtTTzE7AsZ_i_fyZNut8EOfyEc'
+SAMPLE_RANGE_NAME = 'Sheet1!A3:G'
 
 FORMAT = '[%(asctime)s] %(levelname).1s %(message)s'
 
 log_file = 'log.log'
 
-## Некоторые сообщества и люди:
-bt = '-57536014'  # БТ
-mememe = '26546404'  # я сам
-jail = '-92767252'  # Подслушано в Тюрьме
+bt = '-57536014'
+mememe = '26546404'
+jail = '-92767252'
 
 active_scan = bt
 count_of_executes = 5
 
 
 def auth_handler():
-    """ При двухфакторной аутентификации вызывается эта функция.
+    """
     """
 
-    # Код двухфакторной аутентификации
     key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
     remember_device = True
 
     return key, remember_device
@@ -65,7 +59,7 @@ def get_session():
     vk_session = vk_api.VkApi(
         login, password,
 
-        auth_handler=auth_handler  # функция для обработки двухфакторной аутентификации
+        auth_handler=auth_handler
     )
 
     try:
@@ -76,29 +70,8 @@ def get_session():
 
 
 def get_wall(vk_session):
-    """ Пример получения всех комментариев к записям со стены и их ранжирования
-    по сумме лайков на каждого пользователя"""
+
     tools = vk_api.VkTools(vk_session)
-
-    """ VkTools.get_all позволяет получить все объекты со всех страниц.
-        Соответственно get_all используется только если метод принимает
-        параметры: count и offset.
-        Например может использоваться для получения всех постов стены,
-        всех диалогов, всех сообщений, etc.
-        При использовании get_all сокращается количество запросов к API
-        за счет метода execute в 25 раз.
-        Например за раз со стены можно получить 100 * 25 = 2500, где
-        100 - максимальное количество постов, которое можно получить за один
-        запрос (обычно написано на странице с описанием метода)
-
-        https://vk.com/dev/wall.get
-
-        Обратите внимание, идентификатор сообщества в параметре owner_id 
-        необходимо указывать со знаком "-" 
-        — например, owner_id=-1 соответствует идентификатору сообщества ВКонтакте API (club1)
-        Для людей ставить впереди знаки "-" не нужно    """
-
-    # второй параметр тут отвечает за количество запросов в вк через execute. 1 запрос = 25 постов.
     wall = tools.get_all('wall.get', count_of_executes, {'owner_id': active_scan}, limit=1)
 
     return wall, tools
@@ -157,7 +130,7 @@ def parse_post(post) -> list:
 
 
 def supply_post_with_more_data(vk_session, postids):
-    postids = postids[:30] # для api stats есть ограничение в 30 постов в запросе
+    postids = postids[:30]
     vk = vk_session.get_api()
     posts_enriched = vk.stats.getPostReach(owner_id=active_scan, post_ids=postids)
     parsed_post = []
@@ -278,15 +251,14 @@ class Statistics_work():
         cols_to_merge = [0,2,3,4,5,6]
         df_with_parsed_to_merge = df_with_parsed.iloc[:,cols_to_merge]
 
-        df_with_from_sheet['postid'] = df_with_from_sheet['postid'].astype('int32') # возможно это больше не нужно
-        df_with_from_sheet['created_by'] = df_with_from_sheet['created_by'].astype('int32') # возможно это больше не нужно
+        df_with_from_sheet['postid'] = df_with_from_sheet['postid'].astype('int32') # maybe is not needed anymore
+        df_with_from_sheet['created_by'] = df_with_from_sheet['created_by'].astype('int32')
 
         frames = [df_with_parsed, df_with_from_sheet]
         key = ['postid']
 
-        # Объединим новые данные о лайках со старыми авторами
         result = pd.concat(frames, sort=False).groupby(key, as_index=False)['created_by'].max()
-        # И вернем сюда данные о лайках, репостах, просмотрах
+
         result = pd.merge(result, df_with_parsed_to_merge, how='inner', on=key, left_on=None, right_on=None,
                  left_index=False, right_index=False, sort=False,
                  suffixes=('_x', '_y'), copy=True, indicator=False,
@@ -298,18 +270,12 @@ class Statistics_work():
                  suffixes=('_x', '_y'), copy=True, indicator=False,
                  validate=None)
 
-        ## Здесь должна быть проверка на соответствие недельной таймдельте
+        ## Check timedelta for 7 days
 
 
         result = result.sort_values(by=['postid'], ascending=False)
         result.datetimeofpost = result.datetimeofpost.astype(str)
         result = result.values.tolist()
-        """
-        frames = [df_with_parsed, df_with_from_sheet]
-        result = pd.concat(frames)
-        result = result.groupby(['postid', 'created_by']).max()
-        # df_with_parsed = df_with_parsed.loc[df.Weight == "155", "Name"] = "John"
-        """
         return result
 
 
@@ -324,13 +290,11 @@ if __name__ == '__main__':
         print("parsed additional data from VK is ok")
 
         r = Reporting()
-        creds, posts_from_sheets = r._main() # коннектимся к гугл-таблицам и читаем предыдущую статистику
+        creds, posts_from_sheets = r._main()
 
         print("parsed GS is ok")
         s = Statistics_work()
         result = s.compare(posts_from_sheets, parsed, morefeatures)
-        # DFresult = pd.DataFrame(result)
-        # DFresult.to_csv('test2.csv', sep=';')
         r.put(result)
         r.put_last_updated()
 
