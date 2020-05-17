08108efe401e51b411ad5d3af54395da16942123
nejel <nejel@mail.ru>
2020-05-18 00:01:16 +0300

    small fix

--- reporting_to_google_sheets.py
+++ reporting_to_google_sheets.py
@@ -204,7 +204,7 @@ class Reporting:
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials2.json', SCOPES)
+                    'credentials.json', SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open('token.pickle', 'wb') as token:
